43b63a6a035a425a4ed72bf7faf5bcbe10377440
your-name <osherberger@gmail.com>
2024-10-07 13:20:52 +0300

    Adding Inventory list

---
+++ helpers/InventoryList.ts
@@ -0,0 +1,23 @@
+export class PhonesList{
+    public static readonly SAMSUNG_6 = 'Samsung galaxy s6';
+    public static readonly SAMSUNG_7 = 'Samsung galaxy s7';
+    public static readonly NEXUS = 'Nexus 6';
+    public static readonly NOKIA = 'Nokia lumia 1520';
+    public static readonly IPHONE = 'Iphone 6 32gb';
+    public static readonly SONY = 'Sony xperia z5';
+    public static readonly HTC = 'HTC One M9';
+}
+
+export class LaptopsList{
+    public static readonly SONY_5 = 'Sony vaio i5';
+    public static readonly SONY_7 = 'Sony vaio i7';
+    public static readonly MAC_AIR = 'MacBook air';
+    public static readonly MAC_PRO = 'MacBook Pro';
+    public static readonly DELL_7 = 'Dell i7 8gb';
+    public static readonly DELL_2017 = '2017 Dell 15.6 Inch';
+}
+
+export class MonitorsList{
+    public static readonly APPLE = 'Apple monitor 24';
+    public static readonly ASUS = 'ASUS Full HD';
+}
