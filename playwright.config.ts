9d5ad11489eace7b35daaa31b49e6c9412b324e1
your-name <osherberger@gmail.com>
2024-10-13 12:02:57 +0300

    allure report option+ git ignore update

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,8 +18,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: [
+    ['list'],
+    ['allure-playwright'] // Add Allure reporter
+  ],  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
