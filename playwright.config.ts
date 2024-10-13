d49cb123c414fae66501e0a78a1e239bd48b6ee8
your-name <osherberger@gmail.com>
2024-10-13 12:24:43 +0300

    update playwright config ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,30 +1,18 @@
 import { defineConfig, devices } from '@playwright/test';
 
-
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests',
 
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 2,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
-    ['allure-playwright'] // Add Allure reporter
-  ],  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    ['html'], 
+    ['allure-playwright']
+  ], 
   use: {
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
 
     screenshot: 'on',
@@ -34,7 +22,6 @@ export default defineConfig({
     headless: process.env.CI ? true : false,
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
